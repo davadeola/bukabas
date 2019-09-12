48af54a8b1ef0f806b9aa6f6550099abd6b30cad
davadeola <adeoladavid15@gmail.com>
2019-09-12 20:51:58 +0300

    Fixed the reponsiveness on the home page

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,23 @@ import Layout from '../components/Layout'
 const Home=()=>(
     <Layout>
         <div className="landing container-fluid">
-          <img src="/static/images/person.png" id="person" className="home-img"/>
-            <img src="/static/images/lady.png" id="lady" className="home-img"/>
+          <div className="row">
+            <div className="col-md-4"><img src="/static/images/person.png" id="person" className="home-img"/></div>
+            <div className="col-md-4">  <h1 className="display-4">Transforming public transport</h1>
+              <div className='btn-row'>
 
-          <h1 className="display-4">Transforming public transport</h1>
-          <div className='btn-row'>
+                <Link href='/signup'>
+                  <a title='Login page'><button className="btn">Get started today</button></a>
+                </Link>
+              </div></div>
+            <div className="col-md-4">  <img src="/static/images/lady.png" id="lady" className="home-img"/></div>
 
-            <Link href='/signup'>
-              <a title='Login page'><button className="btn">Get started today</button></a>
-            </Link>
           </div>
+
+
+
+
+
 
 
         </div>
