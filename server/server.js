357236ec51d64468f56032a7aa455dfd5d181833
davadeola <adeoladavid15@gmail.com>
2019-09-17 13:30:38 +0300

    Finished creating the backend server

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const express = require('express')
         key: process.env.PUSHER_APP_KEY,
         secret: process.env.PUSHER_APP_SECRET,
         cluster: process.env.PUSHER_APP_CLUSTER,
-        encrypted: true
+        useTLS: true
     });
 
     app.use(bodyParser.json());
@@ -25,3 +25,30 @@ const express = require('express')
         );
         next();
     });
+
+    app.post('/pusher/auth', (req, res) => {
+        let socketId = req.body.socket_id;
+        let channel = req.body.channel_name;
+        random_string = Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0, 5);
+        let presenceData = {
+            user_id: random_string,
+            user_info: {
+                username: '@' + random_string,
+            }
+        };
+        let auth = pusher.authenticate(socketId, channel, presenceData);
+        res.send(auth);
+    });
+
+    app.post('/update-location', (req, res) => {
+        // trigger a new post event via pusher
+        pusher.trigger('presence-channel', 'location-update', {
+            'username': req.body.username,
+            'location': req.body.location
+        })
+        res.json({ 'status': 200 });
+    });
+
+    let port = 3128;
+    app.listen(port);
+    console.log('listening');
