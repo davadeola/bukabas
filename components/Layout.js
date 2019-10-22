2fe60171cc5bcd703221644760383d4a89d75ed3
davadeola <adeoladavid15@gmail.com>
2019-10-22 16:22:38 +0300

    Added a favicon

--- components/Layout.js
+++ components/Layout.js
@@ -12,6 +12,7 @@ const Layout = (props) => (<div>
     <link rel="stylesheet" href="/static/main.css" key="css2"/>
     <link rel="stylesheet" href="https://api.mapbox.com/mapbox-gl-js/v0.42.0/mapbox-gl.css"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <link rel="icon" href="/static/images/logo.png"/>
     <link rel="manifest" href="/static/manifest.json"/>
     <meta name="theme-color" content="#34d7d7"/>
     <link rel="apple-touch-icon" href="/static/images/logo.png"/>
